987b60be0c6b7117ab5c334c31d9547c75b68db1
janayajoo <35385564+janayajoo@users.noreply.github.com>
2023-08-18 22:10:03 -0500

    Se agregan archivos del problema 1

---
+++ Problema1/src/pedido_En_Linea/Pasos_Generales.java
@@ -0,0 +1,20 @@
+package pedido_En_Linea;
+
+public abstract class Pasos_Generales {
+
+    String producto;
+    String iD;
+    String celular;
+    int cantidadSolicitada;
+    int cantidadProductos;
+    Pasos_Generales() {}
+
+    public char informar(){
+        System.out.println("Tu compra se realizo correctamente");
+        return 0;
+    }
+    abstract boolean verificarInventario(int cantidadSolicitada, int cantidadProductos);
+
+    abstract void facturacion(String iD);
+    abstract void notificarCliente(String celular,String iD);
+}
